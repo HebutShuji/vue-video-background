4139e3cb09c508f089bae5296addef85012006a3
enc <enc@encdeMacBook-Air.local>
2020-06-12 10:06:58 +0800

    push

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -62,10 +62,10 @@
 							<el-menu-item index="2-2">选项2</el-menu-item>
 						</el-menu-item-group>
 						<el-menu-item-group title="分组2">
-							<el-menu-item index="2-3">选项3</el-menu-item>
+							<el-menu-item index="2-3">选</el-menu-item>
 						</el-menu-item-group>
 						<el-submenu index="2-4">
-							<template slot="title">选项4</template>
+							<template slot="title">选4</template>
 							<el-menu-item index="2-4-1">选项4-1</el-menu-item>
 						</el-submenu>
 					</el-submenu>
